268ad94f8bffe13c04195b0d493310290b054f4b
ABlanton3 <a.m.blanton@gmail.com>
2020-06-22 11:02:18 -0500

    sure hope I'm doing number 10 right, fixed it a little

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -2,28 +2,28 @@
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
 
-const Amanda = { //object literal
+const amanda = { //object literal
     name: "Amanda",
     age: 28
 }
 
-function person(name, age){//construction function
+function Person(name, age){//construction function
     this.name = name;
     this.age = age
 }
 
-let Anna = new person('Anna', 34)
+let Anna = new Person('Anna', 34)
 
-class Human{
+class Human{ //class
     constructor(name, age){
         this.name = name;
         this.age = age;
     }
 }
 
-const Morgan = new Human("Morgan", 29)
+let Morgan = new Human("Morgan", 29)
 
-console.log(Amanda);
+console.log(`${amanda.name} is ${amanda.age} years old.`);
 console.log(Anna);
 console.log(Morgan);
 
